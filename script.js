e9e6281b3538d02b30bfa7e0283ff3a53752bd84
Tom-77 <131326987+Tom-77@users.noreply.github.com>
2023-06-15 10:40:12 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".slider").bxSlider({
+        mode: 'horizontal',
+        captions: true,
+        //slidewidth: 600,
+        adaptiveHeight: true,
+        auto: true,
+        autoHover: true,
+        pause: 4000
+    });
+});
